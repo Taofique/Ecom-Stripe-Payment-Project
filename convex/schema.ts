43a0e07b079b439b024c370f07513e10091c6f29
Taofique <shornob8@gmail.com>
2026-07-15 21:53:30 +0600

    feat: Implement courses schema in convex

--- convex/schema.ts
+++ convex/schema.ts
@@ -7,4 +7,11 @@ export default defineSchema({
     name: v.string(),
     clerkId: v.string(),
   }).index("by_clerkId", ["clerkId"]),
+
+  courses: defineTable({
+    title: v.string(),
+    description: v.string(),
+    imageUrl: v.string(),
+    price: v.number(),
+  }),
 });
