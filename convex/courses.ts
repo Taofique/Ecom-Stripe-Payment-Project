1c18c54f0d7cbc0c077205c92e55a92a34205691
Taofique <shornob8@gmail.com>
2026-07-25 02:48:17 +0600

    feat: Implement course table setup and fetch courses data from db

---
+++ convex/courses.ts
@@ -0,0 +1,20 @@
+import { query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const getCourses = query({
+  args: {},
+  handler: async (ctx) => {
+    const courses = ctx.db.query("courses").collect();
+    return courses;
+  },
+});
+
+export const getCourseById = query({
+  args: {
+    courseId: v.id("courses"),
+  },
+  handler: async (ctx, args) => {
+    const course = ctx.db.get(args.courseId);
+    return course;
+  },
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,60 @@
-"use client";
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import { ConvexHttpClient } from "convex/browser";
+import { getCourses, getCourseById } from "../../convex/courses";
+import {
+  Card,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  CardContent,
+} from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function Home() {
-  // const tasks = useQuery(api.tasks.getAllTasks);
+export default async function Home() {
+  const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+  const courses = await convex.query(api.courses.getCourses);
 
-  // // Helper function to safely format dates
-  // const formatDate = (date: any) => {
-  //   if (!date) return "No date available";
-  //   try {
-  //     return new Date(date).toDateString();
-  //   } catch (error) {
-  //     return "Invalid date";
-  //   }
-  // };
+  return (
+    <div className=" flex flex-col min-h-screen">
+      <main className="flex-grow container mx-auto px-4 py-16">
+        <div className="text-center mb-16">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-4">
+            Forge Your Path in Modern Development
+          </h1>
+          <p className="text-xl text-muted-foreground max-w-2xl mx-auto ">
+            Master fullstack skills through engaging, project-based learning.
+            Unlock your potential with MasterClass
+          </p>
+        </div>
 
-  return <h1>Hello from NextJS Frontend</h1>;
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-16">
+          {courses.slice(0, 3).map((course) => (
+            <Card key={course._id} className="flex flex-col">
+              <Link
+                href={`/courses/${course._id}`}
+                className="cursor-pointer"
+              />
+
+              <CardHeader>
+                <Image
+                  src={course.imageUrl}
+                  alt={course.title}
+                  width={640}
+                  height={360}
+                  className="rounded-md object-cover"
+                />
+              </CardHeader>
+              <CardContent className="flex-grow">
+                <CardTitle className="text-xl mb-2 hover:underline">
+                  {course.title}
+                </CardTitle>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
