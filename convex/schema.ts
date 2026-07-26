5ba4328a068a207064c1b6f0504a199dd041fe55
Taofique <shornob8@gmail.com>
2026-07-27 01:58:51 +0600

    feat: Implement stripe schema in convex schema.ts

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,11 +1,13 @@
 import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
+import { v, VLiteral } from "convex/values";
 
 export default defineSchema({
   users: defineTable({
     email: v.string(),
     name: v.string(),
     clerkId: v.string(),
+    stripeCustomerId: v.string(),
+    currentSubscriptionId: v.optional(v.id("subscriptions")),
   }).index("by_clerkId", ["clerkId"]),
 
   courses: defineTable({
@@ -14,4 +16,22 @@ export default defineSchema({
     imageUrl: v.string(),
     price: v.number(),
   }),
+
+  purchases: defineTable({
+    userId: v.id("users"),
+    courseId: v.id("courses"),
+    amount: v.number(),
+    purchaseDate: v.number(), // unix timestamp
+    stripePurchaseId: v.string(),
+  }).index("by_userId_and_courseId", ["userId", "courseId"]),
+
+  subscriptions: defineTable({
+    userId: v.id("users"),
+    planType: v.union(v.literal("month"), v.literal("year")),
+    currentPeriodStart: v.number(),
+    currentPeriodEnd: v.number(),
+    stripeSubscriptionId: v.string(),
+    status: v.string(),
+    cancelAtPeriodEnd: v.boolean(),
+  }).index("by_stripeSubscriptionId", ["stripeSubscriptionId"]),
 });
