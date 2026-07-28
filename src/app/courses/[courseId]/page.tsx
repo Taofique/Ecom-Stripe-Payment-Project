19bb857ee965225011340c23f8857b834d401b6e
Taofique <shornob8@gmail.com>
2026-07-28 23:17:43 +0600

    feat: Implement course details card component course locked UI

--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -8,6 +8,8 @@ import { notFound, useParams } from "next/navigation";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Download, FileTextIcon, Lock, PlayCircle } from "lucide-react";
 
 const CourseDetailPage = () => {
   const params = useParams<{ courseId: Id<"courses"> }>();
@@ -49,7 +51,56 @@ const CourseDetailPage = () => {
         </CardHeader>
 
         <CardContent>
-          <CardTitle className="text-3xl mb-4">{courseData.title}</CardTitle>
+          <CardTitle className="text-3xl font-bold mb-4">
+            {courseData.title}
+          </CardTitle>
+
+          {userAccess.hasAccess ? (
+            <>
+              <p className="text-grey-600 mb-6">{courseData.description}</p>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
+                <Button className="flex items-center justify-center space-x-2">
+                  <PlayCircle className="w-5 h-5" />
+                  <span>Start Course</span>
+                </Button>
+
+                <Button
+                  variant="outline"
+                  className="flex items-center justify-center space-x-2"
+                >
+                  <Download className="h-5 w-5" />
+                  <span>Download Materials</span>
+                </Button>
+              </div>
+              <h3 className="text-xl font-semibold mb-4">Course Modules</h3>
+              <ul className="space-y-2">
+                <li className="flex items-center space-x-2">
+                  <FileTextIcon className="size-5 text-gray-400" />
+                  <span>Introduction to Advanced Patterns</span>
+                </li>
+
+                <li className="flex items-center space-x-2">
+                  <FileTextIcon className="size-5 text-gray-400" />
+                  <span>Hooks and Custom Hooks</span>
+                </li>
+              </ul>
+            </>
+          ) : (
+            <div className="text-center">
+              <div className="flex flex-col items-center space-y-4">
+                <Lock className="w-16 h-16 text-gray-400" />
+                <p className="text-lg text-gray-600">This course is locked</p>
+                <p className=" text-gray-500 mb-4">
+                  Enroll in this course to access all premium content
+                </p>
+                <p className="text-2xl font-bold mb-4">
+                  ${courseData.price.toFixed(2)}
+                </p>
+
+                {/* <PurchaseButton courseId={params.courseId} /> */}
+              </div>
+            </div>
+          )}
         </CardContent>
       </Card>
     </div>
