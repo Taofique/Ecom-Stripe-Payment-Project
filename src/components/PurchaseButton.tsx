ce78cff8eddcb57bd4f24bf3e6145356dce8cc4f
Taofique <shornob8@gmail.com>
2026-07-29 21:12:36 +0600

    feat: Implement initial purchaseButton UI design

--- src/components/PurchaseButton.tsx
+++ src/components/PurchaseButton.tsx
@@ -1,8 +1,32 @@
 "use client";
 
 import { Id } from "../../convex/_generated/dataModel";
+import { api } from "../../convex/_generated/api";
+import { useUser } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
+import { Button } from "./ui/button";
 
 const PurchaseButton = ({ courseId }: { courseId: Id<"courses"> }) => {
+  const { user } = useUser();
+
+  const userData = useQuery(
+    api.users.getUserByClerkId,
+    user ? { clerkId: user?.id } : "skip",
+  );
+
+  const userAccess = useQuery(
+    api.users.getUserAccess,
+    userData
+      ? {
+          userId: userData?._id,
+          courseId,
+        }
+      : "skip",
+  ) || { hasAccess: false };
+
+  if (!userAccess.hasAccess) {
+    return <Button variant="outline">Enroll now</Button>;
+  }
   return <div>PurchaseButton</div>;
 };
 
